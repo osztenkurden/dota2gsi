70f738caf03026338fdff4e9c50111867a2a3f8a
Maciej Ziółkowski <maciej.macron.ziolkowski@gmail.com>
2023-10-12 12:01:22 +0200

    Updated item prices and names list

--- types/items_prices.d.ts
+++ types/items_prices.d.ts
@@ -1,7 +1,7 @@
 declare type ItemPriceDefinition = {
-	name: string;
-	origin: string;
-	price: number;
+    name: string;
+    origin: string;
+    price: number;
 };
 export declare const getItem: (itemName: string) => ItemPriceDefinition;
 export {};
